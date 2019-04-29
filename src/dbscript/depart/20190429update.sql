40a529d10874c07886593b52348fa10d1aa0d5a3
lionyu45 <kwgjpdc@hotmail.com>
2019-04-29 14:51:48 +0800

    本次修改，增加sql脚本

---
+++ src/dbscript/depart/20190429update.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.49 : Database - echart
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ 
+alter table t_s_suboffice add pid int(11) default 0 not Null;
+
