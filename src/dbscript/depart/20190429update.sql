140336a3f9f171769a96be91764725a663cf043a
lionyu45 <kwgjpdc@hotmail.com>
2019-05-02 18:50:27 +0800

    提交权限脚本

--- src/dbscript/depart/20190429update.sql
+++ src/dbscript/depart/20190429update.sql
@@ -2,18 +2,19 @@
 SQLyog Ultimate v8.32 
 MySQL - 5.5.49 : Database - echart
 *********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-set global max_allowed_packet = 2*1024*1024*10;
+*/set global max_allowed_packet = 2*1024*1024*10;
 /*执行上述语句后，重启mysql*/
 
+
+
+-- ----------------------------
+-- Table structure for `t_m_user_authority`
+-- ----------------------------
+alter table t_m_user_authority add depid int(11) default NULL;
+
+
+-- ----------------------------
+-- Records of t_m_user_authority
+-- ----------------------------
 alter table t_s_suboffice add pid int(11) default 0 not Null;
 
